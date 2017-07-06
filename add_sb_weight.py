a5eae708f7b1c46c4e3a6e91b53e2fcfa534be7a
Sean-Jiun Wang <churchguitarist@gmail.com>
2017-07-06 17:30:33 +0200

    Update add_sb_weight.py script
    Use the total prefit signal shape rather than the total postfit signal
    shape

--- add_sb_weight.py
+++ add_sb_weight.py
@@ -10,6 +10,8 @@ import numpy
 SIGNAL_SHAPES_PATH = 'vhbb_TH_Znn_13TeV_Signal.root'
 # The name of the signal region bin in the signal region shapes file.
 SIGNAL_SHAPES_BIN = 'Znn_13TeV_Signal'
+# The names of the signal processes.
+SIGNAL_SHAPES_PROCESSES = ['ZH_hbb', 'WH_hbb', 'ggZH_hbb']
 # The path to the mlfit.root file.
 MLFIT_PATH = 'mlfit.root'
 # The name of the signal region bin in the mlfit.root file.
@@ -18,8 +20,18 @@ MLFIT_BIN = 'Znn_SR'
 BDT_BRANCH = 'BDT_Znn_HighPt.Nominal'
 
 
-def get_shape_bin_edges(shapes_path, datacard_bin):
-    """Return an array of the bin edges used by the input shapes.
+def get_histogram_bin_edges(histogram):
+    """Return an array of bin low edges for a histogram.
+    """
+    bin_edges = numpy.array(
+        [histogram.GetXaxis().GetBinLowEdge(i) for i in xrange(1, histogram.GetNbinsX() + 1)],
+        dtype=numpy.float64,
+    )
+    return bin_edges
+
+
+def get_total_signal_prefit(shapes_path, datacard_bin, signals):
+    """Get the total prefit nominal signal shape from the input shapes file.
 
     Parameters
     ----------
@@ -27,29 +39,32 @@ def get_shape_bin_edges(shapes_path, datacard_bin):
         The path to the shapes file.
     datacard_bin : string
         The name of the datacard bin containing the shapes.
+    signals : iterable of strings
+        The names of the signal processes within the datacard.
 
     Returns
     -------
-    bin_edges : numpy.array
-        The array of bin edges.
+    total_signal_prefit : ROOT.TH1F
+        The total prefit nominal signal shape.
     """
     shapes_file = ROOT.TFile.Open(shapes_path)
     shapes_file.cd(datacard_bin)
-    shapes = ROOT.gDirectory.GetListOfKeys()
-    # Since the nominal and varied shapes share the same binning,
-    # take any of the histograms found in the shapes file.
-    shape = ROOT.gDirectory.Get(shapes[0].GetName())
-    bin_edges = numpy.array(
-        [shape.GetXaxis().GetBinLowEdge(i) for i in xrange(1, shape.GetNbinsX() + 1)],
-        dtype=numpy.float64,
-    )
+    signal_shapes = [
+        key.ReadObj() for key in ROOT.gDirectory.GetListOfKeys()
+        if key.GetName() in signals
+    ]
+    for i, shape in enumerate(signal_shapes):
+        if i == 0:
+            total_signal_prefit = shape.Clone('total_signal_prefit')
+        else:
+            total_signal_prefit.Add(shape)
+    total_signal_prefit.SetDirectory(0)
     shapes_file.Close()
-    return bin_edges
+    return total_signal_prefit
 
 
-def get_total_postfit_shapes(mlfit_path, datacard_bin, bin_edges):
-    """Retrun the rebinned the postfit shapes for the total
-    signal and total background from an mlfit.root file.
+def get_total_background_postfit(mlfit_path, datacard_bin, bin_edges):
+    """Get the rebinned total postfit nominal background shape from an mlfit.root file.
 
     Parameters
     ----------
@@ -57,30 +72,26 @@ def get_total_postfit_shapes(mlfit_path, datacard_bin, bin_edges):
         The path to the mlfit.root file.
     datacard_bin : string
         The name of the datacard bin containing the shapes.
-    bin_edges : numpy.array of float
+    bin_edges : numpy.array of floats
         An array of bin low edge values used to rebin the postfit shapes.
 
     Returns
     -------
-    signal_postfit, background_postfit : tuple of ROOT.TH1F
-        The rebinned signal and background postfit shapes.
+    total_background_postfit_rebinned : ROOT.TH1F
+        The rebinned total postfit nominal background shape.
     """
     mlfit_file = ROOT.TFile.Open(mlfit_path)
     mlfit_file.cd('shapes_fit_s/{}'.format(datacard_bin))
-    total_signal = ROOT.gDirectory.Get('total_signal')
-    total_background = ROOT.gDirectory.Get('total_background')
-    total_signal.SetDirectory(0)
-    total_background.SetDirectory(0)
+    total_background_postfit = ROOT.gDirectory.Get('total_background')
+    total_background_postfit_rebinned = ROOT.TH1F('total_background_postfit', '', len(bin_edges) - 1, bin_edges)
+    for i in xrange(1, total_background_postfit_rebinned.GetNbinsX() + 1):
+        total_background_postfit_rebinned.SetBinContent(i, total_background_postfit.GetBinContent(i))
+    total_background_postfit_rebinned.SetDirectory(0)
     mlfit_file.Close()
-    signal_postfit = ROOT.TH1F('signal_postfit', '', len(bin_edges) - 1, bin_edges)
-    background_postfit = signal_postfit.Clone('background_postfit')
-    for i in xrange(1, signal_postfit.GetNbinsX() + 1):
-        signal_postfit.SetBinContent(i, total_signal.GetBinContent(i))
-        background_postfit.SetBinContent(i, total_background.GetBinContent(i))
-    return signal_postfit, background_postfit
+    return total_background_postfit_rebinned
 
 
-def add_sb_weight(src, dst, bdt_branch, signal_postfit, background_postfit):
+def add_sb_weight(src, dst, bdt_branch, total_signal_prefit, total_background_postfit):
     """Add a branch named "sb_weight" which contains the per-event S/(S+B) weight
     for the events' corresponding bin in the signal region BDT score distribution.
     This is to be applied to all MC and data.
@@ -93,10 +104,10 @@ def add_sb_weight(src, dst, bdt_branch, signal_postfit, background_postfit):
         The path to the output ntuple.
     bdt_branch : string
         The name of the branch containing the nominal BDT score.
-    signal_postfit : ROOT.TH1F
-        The postfit total signal shape.
-    background_postfit : ROOT.TH1F
-        The postfit total background shape.
+    total_signal_prefit : ROOT.TH1F
+        The total prefit nominal signal shape.
+    total_background_postfit : ROOT.TH1F
+        The total postfit nominal background shape.
     """
     logger = logging.getLogger('add_sb_weight')
     # Copy any count and weight histograms.
@@ -133,10 +144,10 @@ def add_sb_weight(src, dst, bdt_branch, signal_postfit, background_postfit):
         # Find the BDT bin containing the event. If the event is
         # found in the underflow bin, use the first bin instead.
         bdt_score = bdt_buffer[0] if leaf_index is None else bdt_buffer[leaf_index]
-        bin_index = signal_postfit.FindBin(bdt_score) or 1
+        bin_index = total_signal_prefit.FindBin(bdt_score) or 1
         # Calculate the S/(S+B) weight for the event.
-        s = signal_postfit.GetBinContent(bin_index)
-        b = background_postfit.GetBinContent(bin_index)
+        s = total_signal_prefit.GetBinContent(bin_index)
+        b = total_background_postfit.GetBinContent(bin_index)
         sb_weight[0] = s / (s + b) if b > 0 else 0
         fill_tree_new()
         if i % 1000 == 0 or sb_weight[0] > 10:
@@ -151,9 +162,10 @@ def main():
     python add_sb_weight.py ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8.root ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_new.root
     """
     logging.basicConfig(format='[%(name)s] %(levelname)s - %(message)s', level=logging.INFO)
-    bin_edges = get_shape_bin_edges(SIGNAL_SHAPES_PATH, SIGNAL_SHAPES_BIN)
-    signal_postfit, background_postfit = get_total_postfit_shapes(MLFIT_PATH, MLFIT_BIN, bin_edges)
-    add_sb_weight(sys.argv[1], sys.argv[2], BDT_BRANCH, signal_postfit, background_postfit)
+    total_signal_prefit = get_total_signal_prefit(SIGNAL_SHAPES_PATH, SIGNAL_SHAPES_BIN, SIGNAL_SHAPES_PROCESSES)
+    bin_edges = get_histogram_bin_edges(total_signal_prefit)
+    total_background_postfit = get_total_background_postfit(MLFIT_PATH, MLFIT_BIN, bin_edges)
+    add_sb_weight(sys.argv[1], sys.argv[2], BDT_BRANCH, total_signal_prefit, total_background_postfit)
 
 
 if __name__ == '__main__':
